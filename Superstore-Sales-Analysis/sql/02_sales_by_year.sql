e4497aa4e3bccff1d82ca76a138635d791bbc069
malchi-k <denis.matveev2k16@gmail.com>
2026-03-25 14:05:05 +0300

    Delete Superstore-Sales-Analysis directory

--- Superstore-Sales-Analysis/sql/02_sales_by_year.sql
+++
@@ -1,9 +0,0 @@
--- продажи и прибиль по годам
-SELECT 
-    strftime('%Y', "Order Date") AS year,
-    ROUND(SUM(Sales), 2) AS total_sales,
-    ROUND(SUM(Profit), 2) AS total_profit,
-    ROUND(SUM(Profit)*100.0 / SUM(Sales), 2) AS profit_margin_pct
-FROM orders
-GROUP BY year
-ORDER BY year;
--- Superstore-Sales-Analysis/sql/03_sales_by_category.sql
+++
@@ -1,9 +0,0 @@
--- продажи по категориям 
-SELECT Category, 
-ROUND(SUM(Sales),2) as total_category,
-ROUND(SUM(Profit), 2) AS total_profit,
-ROUND(SUM(Profit)*100 / SUM(Sales), 2) as profit_margin_pct,
-COUNT(DISTINCT "Order ID") as num_counts
-From orders o 
-GROUP BY Category
-ORDER BY total_profit DESC;
